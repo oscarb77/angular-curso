58f5d583966743b026a977abbafb56258dce17de
Oscarb77 <oscarbrs77@gmail.com>
2021-11-30 18:58:01 +0100

    prinmer comiit

---
+++ src/app/heroes/listado/listado.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html',
+
+})
+export class ListadoComponent  {
+
+  heroes: string[]=['Spiderman','Ironman','Hulk','Thor'];
+  heroeBorrado:string="";
+
+  borrarHeroe(){
+    console.log('borrando');
+    this.heroeBorrado=this.heroes.shift()||'';
+    return(this.heroeBorrado);
+  }
+}
